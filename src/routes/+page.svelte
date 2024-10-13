aeeef90399072eb9a6053333a8199657c184c349
lelv <lelv@duck.com>
2024-10-13 20:17:28 +0200

    Feature section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -114,9 +114,9 @@
 
         <!-- Feature section -->
         <div class="mx-auto max-w-2xl px-6 lg:max-w-7xl lg:px-8">
-            <h2 class="text-base/7 font-semibold text-indigo-600">Deploy faster</h2>
+            <h2 class="text-base/7 font-semibold text-indigo-600">Work smarter, not harder</h2>
             <p class="mt-2 max-w-lg text-pretty text-4xl font-medium tracking-tight text-gray-950 sm:text-5xl">
-                Everything you need to deploy your app.</p>
+                Everything you need to manage your wait queue.</p>
             <div class="mt-10 grid grid-cols-1 gap-4 sm:mt-16 lg:grid-cols-6 lg:grid-rows-2">
                 <div class="relative lg:col-span-3">
                     <div class="absolute inset-px rounded-lg bg-white max-lg:rounded-t-[2rem] lg:rounded-tl-[2rem]"></div>
@@ -124,11 +124,10 @@
                         <img class="h-80 object-cover object-left"
                              src="https://tailwindui.com/plus/img/component-images/bento-01-performance.png" alt="">
                         <div class="p-10 pt-4">
-                            <h3 class="text-sm/4 font-semibold text-indigo-600">Performance</h3>
-                            <p class="mt-2 text-lg/7 font-medium tracking-tight text-gray-950">Lightning-fast builds</p>
-                            <p class="mt-2 max-w-lg text-sm/6 text-gray-600">Lorem ipsum dolor sit amet, consectetur
-                                adipiscing elit. In gravida justo et nulla efficitur, maximus egestas sem
-                                pellentesque.</p>
+                            <h3 class="text-sm/4 font-semibold text-indigo-600">Simplicity</h3>
+                            <p class="mt-2 text-lg/7 font-medium tracking-tight text-gray-950">Notify your client on their phone</p>
+                            <p class="mt-2 max-w-lg text-sm/6 text-gray-600">Forget about having to buy and maintain devices for notifying your client.
+                            They already have a device in their pocket that can receive notifications, so let's use that instead!</p>
                         </div>
                     </div>
                     <div class="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 max-lg:rounded-t-[2rem] lg:rounded-tl-[2rem]"></div>
@@ -139,10 +138,10 @@
                         <img class="h-80 object-cover object-left lg:object-right"
                              src="https://tailwindui.com/plus/img/component-images/bento-01-releases.png" alt="">
                         <div class="p-10 pt-4">
-                            <h3 class="text-sm/4 font-semibold text-indigo-600">Releases</h3>
-                            <p class="mt-2 text-lg/7 font-medium tracking-tight text-gray-950">Push to deploy</p>
-                            <p class="mt-2 max-w-lg text-sm/6 text-gray-600">Curabitur auctor, ex quis auctor venenatis,
-                                eros arcu rhoncus massa, laoreet dapibus ex elit vitae odio.</p>
+                            <h3 class="text-sm/4 font-semibold text-indigo-600">Privacy</h3>
+                            <p class="mt-2 text-lg/7 font-medium tracking-tight text-gray-950">No personal information</p>
+                            <p class="mt-2 max-w-lg text-sm/6 text-gray-600">Your client doesn't have to share personal information such as a
+                                telephone number nor email. They only need to access a site on their browser and they're good to go!</p>
                         </div>
                     </div>
                     <div class="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 lg:rounded-tr-[2rem]"></div>
@@ -153,10 +152,10 @@
                         <img class="h-80 object-cover object-left"
                              src="https://tailwindui.com/plus/img/component-images/bento-01-speed.png" alt="">
                         <div class="p-10 pt-4">
-                            <h3 class="text-sm/4 font-semibold text-indigo-600">Speed</h3>
-                            <p class="mt-2 text-lg/7 font-medium tracking-tight text-gray-950">Built for power users</p>
-                            <p class="mt-2 max-w-lg text-sm/6 text-gray-600">Sed congue eros non finibus molestie.
-                                Vestibulum euismod augue.</p>
+                            <h3 class="text-sm/4 font-semibold text-indigo-600">Informational</h3>
+                            <p class="mt-2 text-lg/7 font-medium tracking-tight text-gray-950">Enhance the waiting experience</p>
+                            <p class="mt-2 max-w-lg text-sm/6 text-gray-600">Let your end-user know the average wait time, their current position in the queue,
+                            and send custom messages to their device once the product or service is ready</p>
                         </div>
                     </div>
                     <div class="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 lg:rounded-bl-[2rem]"></div>
@@ -167,11 +166,10 @@
                         <img class="h-80 object-cover object-center"
                              src="https://tailwindui.com/plus/img/component-images/bento-01-integrations.png" alt="">
                         <div class="p-10 pt-4">
-                            <h3 class="text-sm/4 font-semibold text-indigo-600">Integrations</h3>
-                            <p class="mt-2 text-lg/7 font-medium tracking-tight text-gray-950">Connect your favorite
-                                tools</p>
-                            <p class="mt-2 max-w-lg text-sm/6 text-gray-600">Maecenas at augue sed elit dictum
-                                vulputate, in nisi aliquam maximus arcu.</p>
+                            <h3 class="text-sm/4 font-semibold text-indigo-600">Customization</h3>
+                            <p class="mt-2 text-lg/7 font-medium tracking-tight text-gray-950">Make it your own</p>
+                            <p class="mt-2 max-w-lg text-sm/6 text-gray-600">Invite people to your organization, add your company logo to display,
+                                define what information is shared to the end user, and more!</p>
                         </div>
                     </div>
                     <div class="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5"></div>
@@ -182,9 +180,8 @@
                         <img class="h-80 object-cover object-center"
                              src="https://tailwindui.com/plus/img/component-images/bento-01-network.png" alt="">
                         <div class="p-10 pt-4">
-                            <h3 class="text-sm/4 font-semibold text-indigo-600">Network</h3>
-                            <p class="mt-2 text-lg/7 font-medium tracking-tight text-gray-950">Globally distributed
-                                CDN</p>
+                            <h3 class="text-sm/4 font-semibold text-indigo-600">Advanced</h3>
+                            <p class="mt-2 text-lg/7 font-medium tracking-tight text-gray-950">Smart</p>
                             <p class="mt-2 max-w-lg text-sm/6 text-gray-600">Aenean vulputate justo commodo auctor
                                 vehicula in malesuada semper.</p>
                         </div>
