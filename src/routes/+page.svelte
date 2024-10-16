60fcbd14616eb3b511685da1f9da0f770fe4256e
lelv <lelv@duck.com>
2024-10-16 20:05:58 +0200

    Footer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -516,92 +516,26 @@
             </dl>
         </div>
 
-    <!-- Footer -->
-    <div class="mx-auto mt-32 max-w-7xl px-6 lg:px-8">
-        <footer aria-labelledby="footer-heading" class="relative border-t border-gray-900/10 py-24 sm:mt-56 sm:py-32">
-            <h2 id="footer-heading" class="sr-only">Footer</h2>
-            <div class="xl:grid xl:grid-cols-3 xl:gap-8">
-                <img class="h-7" src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
-                     alt="Company name">
-                <div class="mt-16 grid grid-cols-2 gap-8 xl:col-span-2 xl:mt-0">
-                    <div class="md:grid md:grid-cols-2 md:gap-8">
-                        <div>
-                            <h3 class="text-sm font-semibold leading-6 text-gray-900">Solutions</h3>
-                            <ul role="list" class="mt-6 space-y-4">
-                                <li>
-                                    <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Hosting</a>
-                                </li>
-                                <li>
-                                    <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Data
-                                        Services</a>
-                                </li>
-                                <li>
-                                    <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Uptime
-                                        Monitoring</a>
-                                </li>
-                                <li>
-                                    <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Enterprise
-                                        Services</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="mt-10 md:mt-0">
-                            <h3 class="text-sm font-semibold leading-6 text-gray-900">Support</h3>
-                            <ul role="list" class="mt-6 space-y-4">
-                                <li>
-                                    <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Pricing</a>
-                                </li>
-                                <li>
-                                    <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Documentation</a>
-                                </li>
-                                <li>
-                                    <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Guides</a>
-                                </li>
-                                <li>
-                                    <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">API
-                                        Reference</a>
-                                </li>
-                            </ul>
-                        </div>
+        <!-- Footer -->
+        <footer>
+            <div class="mx-auto max-w-7xl overflow-hidden px-6 py-20 sm:py-24 lg:px-8">
+                <nav class="-mb-6 columns-2 sm:flex sm:justify-center sm:space-x-12" aria-label="Footer">
+                    <div class="pb-6">
+                        <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Terms & Conditions</a>
                     </div>
-                    <div class="md:grid md:grid-cols-2 md:gap-8">
-                        <div>
-                            <h3 class="text-sm font-semibold leading-6 text-gray-900">Company</h3>
-                            <ul role="list" class="mt-6 space-y-4">
-                                <li>
-                                    <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">About</a>
-                                </li>
-                                <li>
-                                    <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Blog</a>
-                                </li>
-                                <li>
-                                    <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Jobs</a>
-                                </li>
-                                <li>
-                                    <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Press</a>
-                                </li>
-                                <li>
-                                    <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Partners</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="mt-10 md:mt-0">
-                            <h3 class="text-sm font-semibold leading-6 text-gray-900">Legal</h3>
-                            <ul role="list" class="mt-6 space-y-4">
-                                <li>
-                                    <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Claim</a>
-                                </li>
-                                <li>
-                                    <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Privacy</a>
-                                </li>
-                                <li>
-                                    <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Terms</a>
-                                </li>
-                            </ul>
-                        </div>
+                    <div class="pb-6">
+                        <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Privacy Policy</a>
                     </div>
-                </div>
+                    <div class="pb-6">
+                        <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Refund Policy</a>
+                    </div>
+                    <div class="pb-6">
+                        <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Status</a>
+                    </div>
+                </nav>
+                <p class="mt-10 text-center text-xs leading-5 text-gray-500">&copy; 2024 Your Company, Inc. All rights
+                    reserved.</p>
             </div>
         </footer>
-    </div>
+
 </div>
