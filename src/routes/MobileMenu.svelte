ff3612c6885e6d251e145db1d874a72ebd0a663c
lelv <lelv@duck.com>
2024-10-18 17:01:46 +0200

    Click outside

--- src/routes/MobileMenu.svelte
+++ src/routes/MobileMenu.svelte
@@ -1,6 +1,7 @@
 <script>
     import Transition from "svelte-transition";
     import {createEventDispatcher} from "svelte";
+    import {clickOutside} from "$lib/util/click-outside.js";
 
     const dispatch = createEventDispatcher();
 
@@ -17,9 +18,9 @@
                 leave="transition ease-in-out duration-300 transform"
                 leaveFrom="translate-x-0"
                 leaveTo="translate-x-full">
-        <div class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <div use:clickOutside on:outclick={dispatchCloseEvent}  class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div class="flex items-center justify-between">
-                <a href="#" class="-m-1.5 p-1.5">
+                <a href="/" class="-m-1.5 p-1.5">
                     <span class="sr-only">Your Company</span>
                     <img class="h-8 w-auto"
                          src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600" alt="">
@@ -35,17 +36,17 @@
             <div class="mt-6 flow-root">
                 <div class="-my-6 divide-y divide-gray-500/10">
                     <div class="space-y-2 py-6">
-                        <a href="#"
-                           class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Product</a>
-                        <a href="#"
+                        <a href="#features"
                            class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Features</a>
-                        <a href="#"
-                           class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Marketplace</a>
-                        <a href="#"
-                           class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Company</a>
+                        <a href="#pricing"
+                           class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Pricing</a>
+                        <a href="https://docs.wait-up.net"
+                           class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Documentation</a>
+                        <a href="https://status.wait-up.net"
+                           class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Status</a>
                     </div>
                     <div class="py-6">
-                        <a href="#"
+                        <a href="https://dev.wait-up.net"
                            class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Log
                             in</a>
                     </div>
