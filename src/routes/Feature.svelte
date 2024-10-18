7e4547fd4d64078b8b0b3bbed8cb56531616dcb2
lelv <lelv@duck.com>
2024-10-18 16:24:32 +0200

    To individual eleents

---
+++ src/routes/Feature.svelte
@@ -0,0 +1,87 @@
+<div class="mx-auto max-w-2xl px-6 lg:max-w-7xl lg:px-8">
+    <h2 id="features-header" class="text-base/7 font-semibold text-indigo-600">Work smarter, not harder</h2>
+    <p class="mt-2 max-w-lg text-pretty text-4xl font-medium tracking-tight text-gray-950 sm:text-5xl">
+        Everything you need to manage your wait list.</p>
+    <div class="mt-10 grid grid-cols-1 gap-4 sm:mt-16 lg:grid-cols-6 lg:grid-rows-2">
+        <div class="relative lg:col-span-3">
+            <div class="absolute inset-px rounded-lg bg-white max-lg:rounded-t-[2rem] lg:rounded-tl-[2rem]"></div>
+            <div class="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] max-lg:rounded-t-[calc(2rem+1px)] lg:rounded-tl-[calc(2rem+1px)]">
+                <img class="h-80 object-cover object-left"
+                     src="https://tailwindui.com/plus/img/component-images/bento-01-performance.png" alt="">
+                <div class="p-10 pt-4">
+                    <h3 class="text-sm/4 font-semibold text-indigo-600">Simplicity</h3>
+                    <p class="mt-2 text-lg/7 font-medium tracking-tight text-gray-950">Notify your client on
+                        their phone</p>
+                    <p class="mt-2 max-w-lg text-sm/6 text-gray-600">Forget about having to buy and maintain
+                        devices for notifying your client.
+                        They already have a device in their pocket that can receive notifications, so let's use
+                        that instead!</p>
+                </div>
+            </div>
+            <div class="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 max-lg:rounded-t-[2rem] lg:rounded-tl-[2rem]"></div>
+        </div>
+        <div class="relative lg:col-span-3">
+            <div class="absolute inset-px rounded-lg bg-white lg:rounded-tr-[2rem]"></div>
+            <div class="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] lg:rounded-tr-[calc(2rem+1px)]">
+                <img class="h-80 object-cover object-left lg:object-right"
+                     src="https://tailwindui.com/plus/img/component-images/bento-01-releases.png" alt="">
+                <div class="p-10 pt-4">
+                    <h3 class="text-sm/4 font-semibold text-indigo-600">Privacy</h3>
+                    <p class="mt-2 text-lg/7 font-medium tracking-tight text-gray-950">No personal
+                        information</p>
+                    <p class="mt-2 max-w-lg text-sm/6 text-gray-600">Your client doesn't have to share personal
+                        information such as a
+                        telephone number nor email. They only need to access a site on their browser and they're
+                        good to go!</p>
+                </div>
+            </div>
+            <div class="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 lg:rounded-tr-[2rem]"></div>
+        </div>
+        <div class="relative lg:col-span-2">
+            <div class="absolute inset-px rounded-lg bg-white lg:rounded-bl-[2rem]"></div>
+            <div class="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] lg:rounded-bl-[calc(2rem+1px)]">
+                <img class="h-80 object-cover object-left"
+                     src="https://tailwindui.com/plus/img/component-images/bento-01-speed.png" alt="">
+                <div class="p-10 pt-4">
+                    <h3 class="text-sm/4 font-semibold text-indigo-600">Informational</h3>
+                    <p class="mt-2 text-lg/7 font-medium tracking-tight text-gray-950">Enhance the waiting
+                        experience</p>
+                    <p class="mt-2 max-w-lg text-sm/6 text-gray-600">Let your end-user know the average wait
+                        time, their current position in the list,
+                        and send custom messages to their device once the product or service is ready</p>
+                </div>
+            </div>
+            <div class="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 lg:rounded-bl-[2rem]"></div>
+        </div>
+        <div class="relative lg:col-span-2">
+            <div class="absolute inset-px rounded-lg bg-white"></div>
+            <div class="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)]">
+                <img class="h-80 object-cover object-center"
+                     src="https://tailwindui.com/plus/img/component-images/bento-01-integrations.png" alt="">
+                <div class="p-10 pt-4">
+                    <h3 class="text-sm/4 font-semibold text-indigo-600">Customization</h3>
+                    <p class="mt-2 text-lg/7 font-medium tracking-tight text-gray-950">Make it your own</p>
+                    <p class="mt-2 max-w-lg text-sm/6 text-gray-600">Invite people to your organization, add
+                        your company logo to display,
+                        define what information is shared to the end user, and more!</p>
+                </div>
+            </div>
+            <div class="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5"></div>
+        </div>
+        <div class="relative lg:col-span-2">
+            <div class="absolute inset-px rounded-lg bg-white max-lg:rounded-b-[2rem] lg:rounded-br-[2rem]"></div>
+            <div class="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] max-lg:rounded-b-[calc(2rem+1px)] lg:rounded-br-[calc(2rem+1px)]">
+                <img class="h-80 object-cover object-center"
+                     src="https://tailwindui.com/plus/img/component-images/bento-01-network.png" alt="">
+                <div class="p-10 pt-4">
+                    <h3 class="text-sm/4 font-semibold text-indigo-600">Advanced</h3>
+                    <p class="mt-2 text-lg/7 font-medium tracking-tight text-gray-950">Smart tracking</p>
+                    <p class="mt-2 max-w-lg text-sm/6 text-gray-600">Follow the transition of each entry from
+                        the moment it is being prepared,
+                        once it's ready, and when it has finally concluded.</p>
+                </div>
+            </div>
+            <div class="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 max-lg:rounded-b-[2rem] lg:rounded-br-[2rem]"></div>
+        </div>
+    </div>
+</div>
