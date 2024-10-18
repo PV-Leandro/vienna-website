e64730c1bf77358ec52071180bdd228436ab1c7b
lelv <lelv@duck.com>
2024-10-18 17:19:59 +0200

    Scroll for mobile

--- src/routes/MobileMenu.svelte
+++ src/routes/MobileMenu.svelte
@@ -1,6 +1,7 @@
-<script>
+<script lang="ts">
     import Transition from "svelte-transition";
     import {createEventDispatcher} from "svelte";
+    import {scrollTo} from "$lib/util/scroll";
     import {clickOutside} from "$lib/util/click-outside.js";
 
     const dispatch = createEventDispatcher();
@@ -8,6 +9,12 @@
     function dispatchCloseEvent() {
         dispatch('close');
     }
+
+    function scrollWrapper(id: string) {
+        scrollTo(id);
+        dispatchCloseEvent();
+    }
+
 </script>
 
 <div class="lg:hidden" role="dialog" aria-modal="true">
@@ -18,7 +25,8 @@
                 leave="transition ease-in-out duration-300 transform"
                 leaveFrom="translate-x-0"
                 leaveTo="translate-x-full">
-        <div use:clickOutside on:outclick={dispatchCloseEvent}  class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <div use:clickOutside on:outclick={dispatchCloseEvent}
+             class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div class="flex items-center justify-between">
                 <a href="/" class="-m-1.5 p-1.5">
                     <span class="sr-only">Your Company</span>
@@ -37,8 +45,10 @@
                 <div class="-my-6 divide-y divide-gray-500/10">
                     <div class="space-y-2 py-6">
                         <a href="#features"
+                           on:click={() => scrollWrapper('features-header')}
                            class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Features</a>
                         <a href="#pricing"
+                           on:click={() => scrollWrapper('pricing-header')}
                            class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Pricing</a>
                         <a href="https://docs.wait-up.net"
                            class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Documentation</a>
