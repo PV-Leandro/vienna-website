c236eab83f11acdfbdcda3f7b6b944bc88c4dfb4
lelv <lelv@duck.com>
2024-10-18 19:57:18 +0200

    Scroll on load

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,14 +1,1 @@
-import {browser} from '$app/environment';
-import '$lib/i18n';
-import {locale, waitLocale} from 'svelte-i18n';
-import type {LayoutLoad} from './$types';
-
 export const ssr = true;
-
-
-export const load: LayoutLoad = async () => {
-    if (browser) {
-        locale.set(window.navigator.language)
-    }
-    await waitLocale();
-}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,8 +7,21 @@
     import Navigation from "./Navigation.svelte";
     import MobileMenu from "./MobileMenu.svelte";
     import Transition from "svelte-transition";
+    import {onMount} from 'svelte';
+    import {scrollTo} from "$lib/util/scroll";
 
     let showMobileMenu = false;
+
+    onMount(() => {
+        const fragment = window.location.hash.substring(1);
+        console.log('fragment', fragment);
+        if (fragment === 'pricing') {
+            scrollTo('pricing-header');
+        } else if (fragment === 'features') {
+            scrollTo('features-header');
+        }
+    });
+
 </script>
 
 <div class="bg-white">
